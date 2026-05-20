0106523fa0a9f90e20a86a43f5ef9f02840771da
Yogesh Patidar <ypatidar8815@gmail.com>
2026-05-20 19:20:47 +0530

    best approach

--- src/main/ARRAY/MissingNumber.java
+++ src/main/ARRAY/MissingNumber.java
@@ -3,26 +3,37 @@ package ARRAY;
 public class MissingNumber {
     static int missingElement(int[] arr){
 
-       int num = 0;
+       int totalsum = 0;
+       int presentsum = 0;
 
-       for(int i = 0; i< arr.length; i++){
-           int count = 0;
-           num = i;
-           for(int j = 0; j<arr.length; j++){
-               if(arr[j] == i){
-                   count++;
-                   //do nothing
-               }
-           }
-           if(count != 1){
-               return num;
-           }
-       }
-       return -1;
+//       for(int i = 0; i< arr.length; i++){
+//           int count = 0;
+//           num = i;
+//           for(int j = 0; j<arr.length; j++){
+//               if(arr[j] == i){
+//                   count++;
+//                   //do nothing
+//               }
+//           }
+//           if(count != 1){
+//               return num;
+//           }
+//       }
+
+        for(int i = 0; i<= arr.length; i++){
+            totalsum += i;
+        }
+        for(int i = 0; i<arr.length; i++){
+            presentsum += arr[i];
+        }
+
+        int num = totalsum - presentsum;
+
+       return num;
     }
     static void main() {
 
-        int[] arr = {1,2,3,0,5,6,4,7,9};
+        int[] arr = {1,3,0,5,8,10,6,4,7,9};
 
         int key = missingElement(arr);
 
